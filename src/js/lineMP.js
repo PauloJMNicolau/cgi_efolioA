125623c0556ae440b64061c98c81313d80fd5e48
Paulo Jorge Martins Nicolau <paulojmnicolau@paulo.liberatensystems>
2020-11-19 01:53:43 +0000

    teclas extra iluminação correção algoritmo de brasenham

--- src/js/lineMP.js
+++ src/js/lineMP.js
@@ -1,103 +1,70 @@
   //Ficheiro lineMP.js
 
 function lineMP(pontoA, pontoB){
-    //Copiar variaveis dos argumentos para variaveis auxiliares
     let x1 = pontoA[0];
     let y1 = pontoA[1];
     let x2 = pontoB[0];
     let y2 = pontoB[1];
-    let dx = x2 - x1;                           //Calcular o valor de DeltaX
-    let dy = y2 - y1;                           //Calcular o valor de DeltaY
-    //Reduzir pontos ao 1º octante 
-    //Variavel auxiliar que verifica alteração de valores para o simetrico e declive
-    let simetrico = false;                          
-    let declive = false;                            
-    //Verificar declive da reta negativo
-    let m = dx * dy;
-    console.log(m);
-    if(m < 0){
-        simetrico = true;                           //Existiu Simetria de y
-        y1 = -y1;                                   //Trocar y1
-        y2 = -y2;                                   //Trocar y2
-        dy = -dy;
+    let simetrico = false;                      //Variavel auxiliar para verificar se existiu trocas de valores
+    let declive = false;                        //dos pontos devido a estarem noutro octante que não o padrão
+    let dx = x2 - x1;                           //DeltaX
+    let dy = y2 - y1;                           //DeltaY
+    let m = dx * dy;                            //Declive da reta
+    if(m < 0){                                  //Verifica se declive é negativo
+        simetrico = true;                       //Se for negativo atualiza valor de simetrico
+        dy = -dy;                               //e troca valores de deltay e y1 e y2
+        y1 = -y1;                               //para valores simétricos
+        y2 = -y2;
     }
-    //Verifica valores absolutos dos delta
-    if (Math.abs(dx) < Math.abs(dy)){
-        console.log(true);
-        declive = true;                             //Existiu Troca de Variaveis 
-        let aux = x1;                               //Trocar x1 e x2
-        x1 = x2;
-        x2 = aux;
-        aux = y1;                                   //Trocar y1 e y2
-        y1 = y2;
+    if(Math.abs(dx)< Math.abs(dy)){             //Verifica se o valor (absoluto) de deltaX é infeior ao de deltaY 
+        declive = true;                         //Se for atualiza o valor de declive
+        let aux = x1;                           //e troca a ordem das coordenadas (x,y) para (y,x)
+        x1 = y1;                                //de ambos os pontos
+        x1 = aux;
+        aux = x2;
+        x2 = y2;
         y2 = aux;
-        aux = dx;                                   //Trocar dx e dy
+        aux = dx;                               //e dos delta também
         dx = dy;
         dy = aux;
     }
-    //Verificar se ponto A é superior ao ponto B
-    if(x1 > x2){
-        let aux = x1;                               //Trocar ordem dos pontos de forma a
-        x1 = x2;                                    //que o algoritmo possa calcular pontos
-        x2 = aux;                                   //independentemente da direção
+    if( x1 > x2){                               //verifica se valor de x1 é superior ao de x2 
+        let aux = x1;                           //Caso seja troca a ordem dos pontos 
+        x1 = x2;
+        x2 = aux;
         aux = y1;
         y1 = y2;
         y2 = aux;
-        dx = -dx;
+        dx = -dx;                               //e troca os deltas para os valores simétricos deles
         dy = -dy;
     }
-       
-    //Verificar declive da reta de forma a verificar se  incrementa ou decrementa a variavel
-    /*let movimento = 1;                  //Irá incrementar variável
-    if (dy < 0) {
-        movimento = -1;                 //Irá decrementar variável
-        dy = -dy;
-    }*/
-
-    
-    //Pré calcular valores de incremento
-    
-    let incrementoNE = 2 * (dy - dx);   //Incremento para escolha de NE
-    let incrementoE = 2 * dy;           //Incremento para escolha de E
-    let d = 2 * dy - dx;                //Calcular valor inicial de d
-    //Calcular Pontos
-    let pontos = [];                    //Declarar Array de retorno dos pontos calculados
-    
-    let indice=x1;
-    if(x1 == x2){
-        if(y1 > y2){
-            let aux = y1;
-            y1 = y2;
-            y2 = aux;
-        }
-        let x = x1;
-        for(let y= y1; y <= y2; y++){
-            pontos.push([x,y]);
-        }
-    } else {
-    
-    let y = y1;                         //Variavel auxiliar para ponto y
-    for(let x = x1; x<= x2; x++){
-        let auxX = x;
-        let auxY = y;
-        if(declive ==1){                    //Verifica Troca de coordenadas
-            let aux = auxX;                 //e ajusta caso tenha existido
-            auxX = auxY;
-            auxY = aux;
-        }
-        if(simetrico == 1){                 //Verifica simetria de Y
-            auxY = -auxY;                   //e troca caso tenha existido
-        }
-        pontos.push([auxX,auxY]);             //Adicionar o ponto inicial ao array de pontos calculados
-        //Calcular proximo valor de d
+    let pontos = [];                            //Variável que guarda os pontos calculados
+    let y = y1;                         
+    let incrementoE = 2 * dy;                   //Incremento para E
+    let incrementoNE = 2 * (dy -dx);            //Incremento para NE
+    let d = 2 * dy - dx;                        //Valor de calculo base para os incrementos
+    for (let x = x1; x< x2; x++){               //Calcula os pontos 
+        pontos.push([x,y]);                     
         if(d <= 0){
-            d += incrementoE;
+            d += incrementoE;                   //Se d for negativo ou nulo incrementa d para E
         } else {
-            d += incrementoNE;
-            y++;
-            //y += movimento;
+            y++;                                //Se d for positivo incrementa y 
+            d += incrementoNE;                  //e d incrementa para NE
         }
     }
-}
+    pontos.push([x2,y2]);                       //Adiciona a ultima coordenada da extremidade da linha
+    for(let i = 0; i < pontos.length; i++){     //Percorre o arrei de pontos Para atualizar os valores com base nas variáveis auxiliares
+        let x = pontos[i][0];
+        let y = pontos[i][1];
+        if(declive == true){                    //Verifica se existiu troca de coordenadas    
+            let aux = x;                        //Caso exista volta a trocar x com y (calculados)
+            x = y;
+            y = aux;
+        }
+        if(simetrico == true){                  //Verifica se existiu troca de valores para simétricos
+            y = -y;                             //Caso exista volta a trocar y (calculado) para o seu simétrico
+        }
+        pontos[i]= [x,y];                       //Atualiza os valores no array de pontos
+    }
     return pontos;
 }
